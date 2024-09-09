e77f782439eb57b3534bf5a36bcc32539e830ebd
DESKTOP-AKCFSMB\anaju <anajuliavrodriguez@gmail.com>
2024-09-09 13:45:17 -0300

    msj de ayuda con tooltips y jopnionpanel + arreglo de algunos javadoc. Queda arreglar interfaz, consola y hacer java docs más completos.

--- src/main/java/conexion/Main.java
+++ src/main/java/conexion/Main.java
@@ -3,7 +3,17 @@ package conexion;
 import gui.Login;
 import java.io.IOException;
 
+/**
+ * Clase que da inicio al sistema.
+ * @author Ana, Gabriel, Gonzalo, Juan y Santiago.
+*/
 public class Main {
+    
+    /**
+     * Método que da inicio al programa.
+     * @param args argumentos del main.
+     * @throws IOException si da error de conexión.
+     */
     public static void main(String[] args) throws IOException {
         Login login = new Login();
         login.setVisible(true);
